3490fab2eed31e7819cf118136fd1890dc1f202c
Abdellah Youssfi <abdellah.y.ouahabi@gmail.com>
2022-10-18 19:11:06 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/arrow.svg'
+import './assets/Loader.css'
+
+function App() {
+  const [checked, setChecked] = useState(true);
+
+
+  return (
+    <div>
+     
+      <input id="package" type="checkbox" />
+
+      <input
+        id="two"
+        type="radio"
+        name="size"
+        checked={checked}
+        onChange={() => setChecked(!checked)}
+      />
+      
+      <label htmlFor="two">M</label>
+
+      <label className="close" htmlFor="package">
+        
+      </label>
+      <label className="open" htmlFor="package">
+        
+      </label>
+
+      <div className="scene">
+        <div className="package__wrapper">
+          <div className="package">
+            <div className="package__content">
+              <img
+                className="package__icon package__icon--css"
+                src="easel.png"
+              />
+              <img
+                className="package__icon package__icon--html"
+                src="Brush.png"
+              />
+              <img
+                className="package__icon package__icon--js"
+                src="colorboard.png"
+              />
+            </div>
+
+            <div className="package__side package__side--main">
+              <div className="package__flap package__flap--top">
+                <div className="package__tape package__tape--top"></div>
+              </div>
+              <div className="package__flap package__flap--bottom">
+                <div className="package__tape package__tape--bottom"></div>
+              </div>
+              <div className="package__side package__side--tabbed">
+                <img className="package__branding" src="Recycle.png" />
+                <div className="package__flap package__flap--top"></div>
+                <div className="package__flap package__flap--bottom"></div>
+              </div>
+
+              <div className="package__side package__side--extra">
+                <div className="package__flap package__flap--top"></div>
+
+                <div className="package__flap package__flap--bottom"></div>
+
+                <div className="package__side package__side--flipped">
+                  <span className="package__direction">
+                    <svg viewBox="0 0 256 512">
+                      <path d="M88 166.059V468c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12V166.059h46.059c21.382 0 32.09-25.851 16.971-40.971l-86.059-86.059c-9.373-9.373-24.569-9.373-33.941 0l-86.059 86.059c-15.119 15.119-4.411 40.971 16.971 40.971H88z"></path>
+                    </svg>
+
+                    <span> Loading...</span>
+
+                    <svg viewBox="0 0 256 512">
+                      <path d="M88 166.059V468c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12V166.059h46.059c21.382 0 32.09-25.851 16.971-40.971l-86.059-86.059c-9.373-9.373-24.569-9.373-33.941 0l-86.059 86.059c-15.119 15.119-4.411 40.971 16.971 40.971H88z"></path>
+                    </svg>
+                  </span>
+
+                  {/* <!-- eyes box --> */}
+                  <span className="package__label package__label--shadow"></span>
+                  <span className="package__label package__label--shadow2"></span>
+
+                  {/* <!-- ////////////////// --> */}
+
+                  <div className="package__flap package__flap--top"></div>
+                  <div className="package__flap package__flap--bottom"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div style={{ paddingLeft: "60%", fontSize: "60px" }}>66%</div>
+          <div id="loaderContainer">
+            <div id="loaderBar"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
