0843f522b50d50e735fb2a1650fe61b83c113f29
Abdellah Youssfi <abdellah.y.ouahabi@gmail.com>
2022-11-03 20:02:36 -0400

    adding areas and some 3d models

--- src/Components/OverLay/LeftContent.tsx
+++ src/Components/OverLay/LeftContent.tsx
@@ -4,19 +4,16 @@ import "../../assets/LeftContent.css";
 
 interface LeftProps {
   active: boolean;
-  content?: HTMLElement;
+  headerText?: string;
 }
 
-const LeftContent: FC<LeftProps> = ({ active }) => {
-  
+const LeftContent: FC<LeftProps> = ({ active, headerText }) => {
+  const { setActiveBox, setContentBox } = useGlobalContext();
   return (
-    <div
-      
-      className={active ? "ex  rotate-left-hovered " : "ex  rotate-left"}
-    >
+    <div className={active ? "ex  rotate-left-hovered " : "ex  rotate-left"}>
       <div className={active ? "section-header" : "section-header-nobefore"}>
         <div className="text-wrapper">
-          <h1>BoxFolio</h1>
+          <h1>{headerText}</h1>
           <h3>This is a Boxfolio project designed and developed by A.Yousfi</h3>
         </div>
       </div>
