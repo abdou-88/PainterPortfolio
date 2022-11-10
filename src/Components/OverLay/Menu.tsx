c15c174bfcea8086bb1236b5e43a54e26486e41a
Abdellah Youssfi <abdellah.y.ouahabi@gmail.com>
2022-11-10 07:25:20 -0500

    menu animation done

--- src/Components/OverLay/Menu.tsx
+++ src/Components/OverLay/Menu.tsx
@@ -10,7 +10,7 @@ interface MenuProps {
 }
 
 const Menu: FC<MenuProps> = ({ active }) => {
-  const [open, setOpen] = useState(active);
+  const [open, setOpen] = useState(true);
   return (
     <>
       <img
@@ -19,13 +19,49 @@ const Menu: FC<MenuProps> = ({ active }) => {
       />
       <div
         id="nav-icon"
-        className={open ? "" : "open"}
+        className={open ? "open" : ""}
         onClick={() => setOpen(!open)}
       >
         <span></span>
         <span></span>
         <span></span>
+
+
+        <div className={open ? "mcd-menu opened" : "mcd-menu"} >
+       
+          <div style={{ left: "100px" }}>
+            <a href="" className="" >
+              <strong>home</strong>
+              <small>---------------</small>
+            </a>
+          </div>
+          <div style={{ left: "170px" }}>
+            <a href="" className="" >
+              <strong>About me</strong>
+              <small>--------------------------</small>
+            </a>
+          </div>
+          <div style={{ left: "275px" }}>
+            <a href="" className="" >
+              <strong>news</strong>
+              <small>--------------</small>
+            </a>
+          </div>
+          <div style={{ left: "340px" }}>
+            <a href="" className="active" >
+              <strong>media</strong>
+              <small>----------------</small>
+            </a>
+          </div>
+          <div style={{ left: "410px" }}>
+            <a href="" className="" >
+              <strong>contact</strong>
+              <small>------------------------</small>
+            </a>
+          </div>
+        </div>
       </div>
+      
     </>
   );
 };
