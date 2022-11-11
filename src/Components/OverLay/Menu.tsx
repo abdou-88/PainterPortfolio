81671fdef9a10189806d82b26d1bf5556a67f94e
Abdellah Youssfi <abdellah.y.ouahabi@gmail.com>
2022-11-11 15:42:27 -0500

    fixing the menu onclick active item

--- src/Components/OverLay/Menu.tsx
+++ src/Components/OverLay/Menu.tsx
@@ -1,7 +1,6 @@
-import  { FC, useState } from "react";
+import { FC, useState } from "react";
 
 import menuBGUp from "/menubgUp.png";
-import menuBGUpleft from "/menubgUpleft.png";
 
 import "../../assets/menu.css";
 
@@ -9,61 +8,59 @@ interface MenuProps {
   active: boolean;
 }
 
+
 const Menu: FC<MenuProps> = ({ active }) => {
   const [open, setOpen] = useState(true);
+
+  const [activeItem, setActiveItem] = useState([false, true, false, false, false]);
+
+
+  const MBclick = (index: number) => {
+    let items = [false, false, false, false, false];
+    items[index] = true;
+    setActiveItem(items);
+    
+
+  }
+  const MButton = (name: string, active: boolean, index: number) => {
+    return (
+      <div onClick={(e) => { MBclick(index) }}>
+        <a href="#" className={active ? "active" : ""} >
+          <strong>{name}</strong>
+          <small>------------------------</small>
+        </a>
+      </div>
+
+    );
+  }
+
   return (
     <>
       <img
         className={active ? "menuBGUp-hovered" : "menuBGUp"}
         src={menuBGUp}
       />
+
       <div
         id="nav-icon"
         className={open ? "open" : ""}
-        onClick={() => setOpen(!open)}
-      >
+        onClick={() => setOpen(!open)}>
         <span></span>
         <span></span>
         <span></span>
-
-
-        <div className={open ? "mcd-menu opened" : "mcd-menu"} >
-       
-          <div style={{ left: "100px" }}>
-            <a href="" className="" >
-              <strong>home</strong>
-              <small>---------------</small>
-            </a>
-          </div>
-          <div style={{ left: "170px" }}>
-            <a href="" className="" >
-              <strong>About me</strong>
-              <small>--------------------------</small>
-            </a>
-          </div>
-          <div style={{ left: "275px" }}>
-            <a href="" className="" >
-              <strong>news</strong>
-              <small>--------------</small>
-            </a>
-          </div>
-          <div style={{ left: "340px" }}>
-            <a href="" className="active" >
-              <strong>media</strong>
-              <small>----------------</small>
-            </a>
-          </div>
-          <div style={{ left: "410px" }}>
-            <a href="" className="" >
-              <strong>contact</strong>
-              <small>------------------------</small>
-            </a>
-          </div>
-        </div>
       </div>
-      
+
+
+      <div className={open ? "mcd-menu opened" : "mcd-menu"} >
+        {MButton("Home", activeItem[0], 0)}
+        {MButton("About me", activeItem[1], 1)}
+        {MButton("news", activeItem[2], 2)}
+        {MButton("media", activeItem[3], 3)}
+        {MButton("contact", activeItem[4], 4)}
+      </div>
     </>
   );
 };
 
 export default Menu;
+
