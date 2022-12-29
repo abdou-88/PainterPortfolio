fa3846d126f846ed27736bfe649883ac6d7a47e8
Abdellah Youssfi <abdellah.y.ouahabi@gmail.com>
2022-12-29 08:42:46 -0500

    arriw timeline

--- src/Components/ScrollTimeLine.tsx
+++ src/Components/ScrollTimeLine.tsx
@@ -10,34 +10,36 @@ import arrow from '/arrowTimeLine.png';
 
 
 
-const timeLineBalls = (n:number, onClick:Function, current:number) => (
-  
+const timeLineBalls = (n: number, onClick: Function, current: number) => (
+
     Array(n).fill(0).map((i, index) => (
         <div
             key={index}
-            className={`timeline__ball ${current >= (index/(n-1)) ? 'active' : null}`}
+            className={`timeline__ball ${current >= (index / (n - 1)) ? 'active' : null}`}
             onClick={() => onClick((index) / (n - 1))}
         >
-            <img className="iconTimeLine" src={current >= (index / (n - 1)) ? iconsOpened :  iconsClosed}/>
+            <img className="iconTimeLine" src={current >= ((index / (n - 1))-0.01) ? iconsOpened : iconsClosed} />
         </div>
     ))
 );
 
 const ScrollTimeLine: FC<{}> = () => {
-    
+
     const { scOffSet, setScOffSet } = useGlobalContext();
 
-    
+
     const intermediaryBalls = 5;
-    const calculatedWidth = (scOffSet ) * 100
+    const calculatedWidth = ((scOffSet) * 100) 
 
     return (
-        
+
         <div className="timeline">
-            <div className="timeline__progress" style={{ width: `${calculatedWidth}%` }} ><img style={{ float: "right", position: "relative", width: "30px", height: "30px" }} src={arrow}/></div>
-            {timeLineBalls(intermediaryBalls , setScOffSet, scOffSet)}
+            <div className="timeline__progress" style={{ width: `${calculatedWidth}%` }} >
+                <img style={{ float: "right", position: "relative", width: "30px", height: "30px" }} src={arrow} />
+            </div>
+            {timeLineBalls(intermediaryBalls, setScOffSet, scOffSet)}
         </div>
-      
+
     );
 };
 
